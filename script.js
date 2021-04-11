62c4dd8f98bede37243b620146fcbbbabced9c0f
seijihosokawa <seijihosokawa@gmail.com>
2021-04-11 17:43:17 -0400

    added bootstrap and style selection

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var stage = new NGL.Stage("viewport");
-  stage.loadFile("rcsb://proteins/6WZR", {defaultRepresentation: true});
-});
+// create a `stage` object
+var stage = new NGL.Stage("viewport");
+// load a PDB structure and consume the returned `Promise`
+function load_pdb(){
+    stage.loadFile("rcsb://pdb_files/6WZR").then(function (component) {
+        // add a "cartoon" representation to the structure component
+        var e = document.getElementById("style");
+        var style_val = e.options[e.selectedIndex].text;
+        console.log("style", style_val);
+        component.addRepresentation(style_val);
+        // provide a "good" view of the structure
+        component.autoView();
+    });
+}
+
+
