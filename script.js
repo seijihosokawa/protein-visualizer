1a86f104b524ca614094d40560f0a6d9b4fb25c3
Yuuki <yuukihosok@gmail.com>
2021-04-28 21:08:22 +0100

    d

--- script.js
+++ script.js
@@ -338,8 +338,6 @@ function takeScreenshot(){
 
 //tasks:
 // examples of proteins with animations + highlighted parts of hemoglobin, covid19, etc (https://github.com/nglviewer/ngl/blob/v0.9.3/examples/js/examples.js)
-// examples of proteins with animations + highlighted parts of hemoglobin, covid19, etc
-// notification (maybe green) that shows for a couple seconds as confirmation of loading or something
 // textbox showing important information such as current loaded protein
 // add tooltips ref: https://getbootstrap.com/docs/4.0/components/tooltips/
 // https://proteopedia.org/wiki/fgij/
